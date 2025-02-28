d9d132ad3591fa5312bf8851d18f2bebabc1b790
AlexGudis <gudis2003@mail.ru>
2025-02-28 16:12:21 +0300

    My character added

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -13,10 +13,11 @@ character = cowsay.read_dot_cow(StringIO("""
 $the_cow = <<EOC;
          $thoughts
           $thoughts
-           ___
-          (o o)
-         (  V  )
-        /--m-m-
+                                        
+         (\_ _/)
+        ( ' o ' )
+        (  -'-  )~~~
+         `-----'
 EOC
 """))
 
