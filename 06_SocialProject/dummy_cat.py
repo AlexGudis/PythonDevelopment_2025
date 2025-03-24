88a830877b7c6a7a09960d083c411a4f367ade34
AlexGudis <gudis2003@mail.ru>
2025-03-25 01:06:23 +0300

    who command

---
+++ 06_SocialProject/dummy_cat.py
@@ -0,0 +1,58 @@
+import sys
+import socket
+import threading
+import cmd
+import readline
+import queue
+
+quе = queue.Queue()
+
+class chat(cmd.Cmd):
+    prompt = 'input>>> '
+
+    def __init__(self, *args, socket, **kwargs):
+        self.s = socket
+        return super().__init__(*args, **kwargs)
+    
+    def do_cows(self, args):
+        self.s.sendall("0 cows\n".encode())
+
+    def do_who(self, arg):
+        s.sendall("0 who\n".encode())
+
+    def do_login(self, args):
+        s.sendall(f"0 login {args}\n".encode())
+
+    def complete_login(self, text, line, begidx, endidx):
+        s.sendall(f"1 cows\n".encode())
+        tokens = (line + ".").split()
+        return [c for c in queue.get().split(", ") if c.startswith(tokens[-1][:-1])]
+
+    def do_eof(self, *args):
+        return 1
+
+
+    def msg_processing(self, cmdline, s):
+        while True:
+            response = s.recv(1024).rstrip().decode()
+            if response[0] == '0':
+                print(f"\n{response[2:]}\n{cmdline.prompt}{readline.get_line_buffer() if readline.get_line_buffer() else ""}",
+                    end="", flush=True)
+            else:
+                queue.put(response[2:])
+    
+
+
+
+
+
+if __name__ == '__main__':
+    host = "localhost" if len(sys.argv) < 2 else sys.argv[1]
+    port = 1337 if len(sys.argv) < 3 else int(sys.argv[2])
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    cmdline = chat(socket=s)
+    mes = threading.Thread(target=cmdline.msg_processing, args = (cmdline, s))
+    mes.start()
+    cmdline.cmdloop()
+    s.shutdown(socket.SHUT_RDWR)
---
+++ 06_SocialProject/prog.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+import asyncio
+import shlex
+import cowsay
+
+class User:
+    def __init__(self):
+        self.name = None
+        self.que = asyncio.Queue()
+
+clients = {}
+
+async def chat(reader, writer):
+    test_connect = "{}:{}".format(*writer.get_extra_info('peername'))
+    print(test_connect)
+
+
+    user = User()
+
+    send = asyncio.create_task(reader.readline())
+    receive = asyncio.create_task(user.que.get())
+    while not reader.at_eof():
+        done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+        for q in done:
+            if q is send:
+                send = asyncio.create_task(reader.readline())
+
+                line = q.result().decode()
+                line = shlex.split(line)
+
+                print(f'Got line = {line}')
+
+                match line:
+                    case [id, 'login', cow]:
+                        if cow not in cowsay.list_cows():
+                            await user.que.put("You cannot choose this login. Please check cows command")
+                        elif cow not in clients.keys():
+                            user.name = cow
+                            clients[user.name] = user.que
+                        else:
+                            await user.que.put("This login is already assigned. Please chose another one from cows command")
+
+                    case [id, 'who']:
+                        await user.que.put(f'{id} {", ".join(clients.keys())}')
+
+                    case ['quit']:
+                            send.cancel()
+                            receive.cancel()
+                            print(user.name, "DONE")
+                            del clients[user.name]
+                            writer.close()
+                            await writer.wait_closed()
+                    case [id, 'cows']:
+                        already = set(clients.keys()) 
+                        free = set(cowsay.list_cows()) - already
+                        await user.que.put(f'{id} {", ".join(list(free))}')
+
+                    case ['yield', *msg]:
+                        if user.name not in clients.keys():
+                            await user.que.put("You are not signed in yet. Please login.")
+                        else:
+                            for out in clients.values():
+                                if out is not clients[user.name]:
+                                    await out.put(f"{user.name} {' '.join(msg)}")
+                            
+
+                    case ['say', to, *msg]:
+                        if user.name not in clients.keys():
+                            await user.que.put("You are not signed in yet. Please login.")
+                        elif to not in clients.keys():
+                            await user.que.put("This user is not signed in yet")
+                        else:
+                            await clients[to].put(f"{user.name} {' '.join(msg)}")
+                    
+                    case [_]:
+                        await user.que.put("This isn't a valid command!")
+
+
+
+            elif q is receive:
+                receive = asyncio.create_task(user.que.get())
+                writer.write(f"{q.result()}\n".encode())
+                await writer.drain()
+
+async def main():
+    server = await asyncio.start_server(chat, '0.0.0.0', 1337)
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
